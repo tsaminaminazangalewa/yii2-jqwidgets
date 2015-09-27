acc3d2d5072df9a28ed572d00d3d6e1b7cc23171
tsaminaminazangalewa <tsaminaminazangalewa@yahoo.com>
2015-09-27 20:56:08 +0700

    button widget

---
+++ jqxProgressBar.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace tsaminaminazangalewa\jqwidgets;
+
+use yii\helpers\Html;
+
+/**
+ * Button renders a bootstrap button.
+ *
+ * For example,
+ *
+ * ```php
+ * echo Button::widget([
+ *     'label' => 'Action',
+ *     'options' => ['class' => 'btn-lg'],
+ * ]);
+ * ```
+ * @see http://getbootstrap.com/javascript/#buttons
+ * @author Antonio Ramirez <amigo.cobos@gmail.com>
+ * @since 2.0
+ */
+class jqxProgressBar extends Widget
+{
+    /**
+     * @var string the tag to use to render the button
+     */
+    public $tagName = 'div';
+    /**
+     * Initializes the widget.
+     * If you override this method, make sure you call the parent implementation first.
+     */
+    public function init()
+    {
+        parent::init();
+        //$this->clientOptions = false;
+        //Html::addCssClass($this->options, 'btn');
+    }
+
+    /**
+     * Renders the widget.
+     */
+    public function run()
+    {
+        $this->registerPlugin('jqxProgressBar');
+        $this->registerJS('jqxprogressbar');
+        $this->registerTheme();
+        
+        return Html::tag($this->tagName, '', $this->options);
+    }
+}
---
+++ jqxRepeatButton.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace tsaminaminazangalewa\jqwidgets;
+
+use yii\helpers\Html;
+
+/**
+ * Button renders a bootstrap button.
+ *
+ * For example,
+ *
+ * ```php
+ * echo Button::widget([
+ *     'label' => 'Action',
+ *     'options' => ['class' => 'btn-lg'],
+ * ]);
+ * ```
+ * @see http://getbootstrap.com/javascript/#buttons
+ * @author Antonio Ramirez <amigo.cobos@gmail.com>
+ * @since 2.0
+ */
+class jqxRepeatButton extends Widget
+{
+    /**
+     * @var string the tag to use to render the button
+     */
+    public $tagName = 'button';
+    /**
+     * @var string the button label
+     */
+    public $label = 'Button';
+    /**
+     * @var boolean whether the label should be HTML-encoded.
+     */
+    public $encodeLabel = true;
+    /**
+     * Initializes the widget.
+     * If you override this method, make sure you call the parent implementation first.
+     */
+    public function init()
+    {
+        parent::init();
+        //$this->clientOptions = false;
+        //Html::addCssClass($this->options, 'btn');
+    }
+
+    /**
+     * Renders the widget.
+     */
+    public function run()
+    {
+        $this->registerPlugin('jqxRepeatButton');
+        $this->registerJS('jqxbuttons');
+        $this->registerTheme();
+        
+        return Html::tag($this->tagName, $this->encodeLabel ? Html::encode($this->label) : $this->label, $this->options);
+    }
+}
